7c82ea6a05a5be29765308f3cc91f1a5f95490e1
bruce hou <bruce@bruces-MacBook-Pro.local>
2020-11-03 09:24:52 -0500

    linting and formatting

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,11 +1,11 @@
 import pytest
 import json
-import os
 from pathlib import Path
 
 from project.app import app, db
 
 TEST_DB = "test.db"
+
 
 @pytest.fixture
 def client():
@@ -74,9 +74,10 @@ def test_messages(client):
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
 
+
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 0
     login(client, app.config["USERNAME"], app.config["PASSWORD"])
